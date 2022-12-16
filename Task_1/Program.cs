f46b685345131dcf01aed26d9edde212d679f409
unknown <itosha35@gmail.com>
2022-12-16 22:59:10 +0300

    Updated to be in full up with Task requirements

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,10 +2,10 @@
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input B = ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a>b) 
-    Console.WriteLine("A > B");
-if (a<b)
-    Console.WriteLine("A < B");
+if (a>b)
+    Console.WriteLine("A > B : " + Convert.ToString(a)+" > "+Convert.ToString(b));
+else if (a<b)
+    Console.WriteLine("A < B : "+ Convert.ToString(a)+" < "+Convert.ToString(b));
 else
-    Console.WriteLine("A = B");
+    Console.WriteLine("A = B : "+ Convert.ToString(a)+" = "+Convert.ToString(b));
 
