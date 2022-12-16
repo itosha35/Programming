608cfcf7cdf5d404e15d8d7693a842d8a89701a8
unknown <itosha35@gmail.com>
2022-12-16 22:45:47 +0300

    Task_1 Written

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input A = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input B = ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b) 
+    Console.WriteLine("A > B");
+if (a<b)
+    Console.WriteLine("A < B");
+else
+    Console.WriteLine("A = B");
+
