8b43b45e32a9ef4546ef1f161f2f98d7e90dfe90
unknown <itosha35@gmail.com>
2022-12-19 21:41:00 +0300

    Seminar 2 added

---
+++ Seminar_2/Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿bool Compare(int num1, int num2)
+{
+    return num1 % num2 == 0;
+}
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"First number is {a}, second number is {b}, {Compare(a,b)}");
+
---
+++ Seminar_2/Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿bool SqrtAB(int num1, int num2)
+{
+    if (num2 == num1*num1 || num1 == num2*num2) return true;
+    else return false;
+}
+
+Console.WriteLine("Вводи числа A B");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"First number is {a}, second number is {b}, {SqrtAB(a,b)}");
