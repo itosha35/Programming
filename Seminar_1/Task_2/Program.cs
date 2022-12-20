78793046aa1066137b77dd0a94e57cc500c20418
unknown <itosha35@gmail.com>
2022-12-20 23:54:29 +0300

    seminar_1 folder

---
+++ Seminar_1/Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Input A = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input B = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input C = ");
+int c = Convert.ToInt32(Console.ReadLine());
+if ((a>b)&(a>c))
+    Console.WriteLine("A -> MAX = " + Convert.ToString(a));
+else if ((b>a)&(b>c))
+    Console.WriteLine("B -> MAX = " + Convert.ToString(b));
+else if ((c>b)&(c>a)) 
+    Console.WriteLine("C -> MAX = " + Convert.ToString(c));
+else  
+{
+ if ((a==b)&(b==c))
+    Console.WriteLine("All are equal = " + Convert.ToString(a));
+ else if ((a==b)&(b!=c))
+    {
+    if (b>c) Console.WriteLine("A = B -> MAX = " + Convert.ToString(b));
+    else Console.WriteLine("C -> MAX = " + Convert.ToString(c));
+    }
+ else
+ {
+    if (a>c) Console.WriteLine("A -> MAX = " + Convert.ToString(a));
+    else Console.WriteLine("B = C -> MAX = " + Convert.ToString(c));
+ }  
+}
---
+++ Seminar_1/Task_3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Input Number = ");
+int numb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Convert.ToString(numb)+" - Четное?");
+Console.WriteLine(numb % 2 == 0 ? "Да" : "Нет");
