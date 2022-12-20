6f34dadc0b9216575c989968e3ef3c75513982c5
unknown <itosha35@gmail.com>
2022-12-21 02:25:12 +0300

    Task 15 solved

--- Seminar_2/Task_15/Program.cs
+++ Seminar_2/Task_15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool Vacation(int num)
+{
+ if (num == 6 || num == 7)
+    return true;
+else return false;
+}
+
+bool check = false;
+int day = 0;
+while (!check)
+{
+ Console.WriteLine("Input week day number");
+ day = Convert.ToInt32(Console.ReadLine());
+ if (day > 0 && day < 8)
+    check=true;
+}
+Console.WriteLine( Vacation(day) == true ? "YES!" : "NO, keep working!");
