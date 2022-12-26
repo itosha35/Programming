1caa20cee140ecd80999c7de4cb8d4b842f5a658
unknown <itosha35@gmail.com>
2022-12-27 02:05:16 +0300

    Task 19 has been modified. Extra code deleted

---
+++ Seminar_3/Task_19_mod/Program.cs
@@ -0,0 +1,26 @@
+﻿bool Palindrom(int number)
+{
+    int temp = number;
+    int revers = 0;
+    while (temp / 10 > 0)
+        {
+            revers = revers*10 + temp % 10;
+            temp = temp / 10;
+        }  
+     revers = revers*10 + temp % 10;
+    if (number == revers)
+            return true;
+        else return false;
+}
+
+int num = 0;
+Console.WriteLine("PALINDROM");
+Console.WriteLine("NOTE : set NEGATIVE number to ESCAPE");
+
+while (num >= 0)
+{
+    Console.WriteLine("Please, Input number:");
+    num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Number is {num}, Check for Palindrom = ");
+    Console.WriteLine(Palindrom(num) ? "YES! Number is Palindrom!" : " NO! Number is not Palindrom!");
+}
