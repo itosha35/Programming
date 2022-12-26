d95018f8554b53bfcd021e484ec560dc549291ba
unknown <itosha35@gmail.com>
2022-12-26 19:49:35 +0300

    Task 19 updated, Task 21 added

--- Seminar_3/Task_19/Program.cs
+++ Seminar_3/Task_19/Program.cs
@@ -1,25 +1,25 @@
 ﻿bool Palindrom(int number)
 {
-  int decim = 10;
- int temp = number;
- int revers = 0;
- while (number % decim < number)
-    decim*= 10;         
- decim = decim/10;   
- if (decim < 10)
-    return true;
- else 
- {
-  while (decim >= 1)
-   {
-    revers = revers + temp % 10 * decim;
-    temp = temp / 10;
+    int decim = 10;
+    int temp = number;
+    int revers = 0;
+    while (number % decim < number)
+        decim *= 10;
     decim = decim / 10;
-   } 
- if (number == revers)
-   return true;
- else return false;
- }  
+    if (decim < 10)
+        return true;
+    else
+    {
+        while (decim >= 1)
+        {
+            revers = revers + temp % 10 * decim;
+            temp = temp / 10;
+            decim = decim / 10;
+        }
+        if (number == revers)
+            return true;
+        else return false;
+    }
 }
 
 int num = 0;
@@ -28,8 +28,8 @@ Console.WriteLine("NOTE : set NEGATIVE number to ESCAPE");
 
 while (num >= 0)
 {
- Console.WriteLine("Please, Input number:");
- num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Number is {num}, Check for Palindrom = ");
-Console.WriteLine( Palindrom(num) ? "YES! Number is Palindrom!" : " NO! Number is not Palindrom!");
+    Console.WriteLine("Please, Input number:");
+    num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Number is {num}, Check for Palindrom = ");
+    Console.WriteLine(Palindrom(num) ? "YES! Number is Palindrom!" : " NO! Number is not Palindrom!");
 }
