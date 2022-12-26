542b398eb61d42a31765a3bcb40590f033a442b9
unknown <itosha35@gmail.com>
2022-12-26 20:09:48 +0300

    Task 23 updated

--- Seminar_3/Task_23/Program.cs
+++ Seminar_3/Task_23/Program.cs
@@ -3,10 +3,10 @@
     int i = 1;
     while (i < num)
     {
-        Console.Write($" {Math.Pow(i,3)} :");
-        i+=1;
+        Console.Write($" {Math.Pow(i, 3)} :");
+        i += 1;
     }
-    Console.Write($" {Math.Pow(num,3)}");
+    Console.Write($" {Math.Pow(num, 3)}");
 }
 
 Console.WriteLine("Program sets all Cubics from 1 to N");
