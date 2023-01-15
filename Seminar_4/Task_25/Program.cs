e3e40a1083eaf4980cbd432a363a5a29a894f9f1
unknown <itosha35@gmail.com>
2023-01-15 22:37:50 +0300

    Seminar 4 added

---
+++ Seminar_4/Task_25/Program.cs
@@ -0,0 +1,13 @@
+﻿int PowerN(int n, int b)
+{
+    int rezult = 1;
+    for (int i = 1; i <= b; i++)
+        rezult *= n;
+    return rezult;
+}
+Console.WriteLine("Power N program");
+Console.Write("Please, set N = ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please, set power to raise A = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The Power {b} of {num} is {PowerN(num, b)}");
