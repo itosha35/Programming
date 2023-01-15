6338402d9c2e1831a60fa19dff62ceca5734cc5c
unknown <itosha35@gmail.com>
2023-01-15 23:24:45 +0300

    Task 27resolved:

---
+++ Seminar_4/Task_27/Program.cs
@@ -0,0 +1,14 @@
+﻿int Summ(int num)
+{
+    int summa = 0;
+    while (num != 0)
+    {
+        summa = summa + num % 10;
+        num /= 10;
+    }
+    return summa;
+}
+Console.WriteLine("Summa of digits in Number");
+Console.Write("Please, set the Number = ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The summa of digits in {number} is {Summ(number)}");
