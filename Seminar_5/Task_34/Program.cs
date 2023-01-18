3e9e3d646fb7538a35d31cf78ec1f8a7001669d0
unknown <itosha35@gmail.com>
2023-01-18 19:12:14 +0300

    Tasks 34 and 36 completed

---
+++ Seminar_5/Task_34/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] SetMas(int size)
+{
+    int[] mas = new int[size];
+    for (int i = 0; i < size; i++)
+        mas[i] = new Random().Next(100, 1000);
+    return mas;
+}
+void ShowMas(int[] mas)
+{
+    Console.Write("[");
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (i != mas.Length - 1) Console.Write($"{mas[i]},");
+        else Console.Write($"{mas[i]}]");
+    }
+    Console.WriteLine();
+}
+int CountEven(int[] mas)
+{
+    int count = 0;
+    for (int i = 0; i < mas.Length; i++)
+        if (mas[i] % 2 == 0) count++;
+    return count;
+}
+Console.WriteLine("Program returnes count of even numbers in Array Please");
+Console.Write("Please, Input Size of Array = ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = SetMas(size);
+ShowMas(array);
+Console.WriteLine($"In this Array we have: {CountEven(array)} even elements");
---
+++ Seminar_5/Task_38/Program.cs
@@ -0,0 +1,25 @@
+﻿double[] SetMas(int size, int min, int max)
+{
+    int[] mas = new int[size];
+    for (int i = 0; i < size; i++)
+        mas[i] = new Random().Next(min, max + 1);
+    return mas;
+}
+void ShowMas(int[] mas, char symb)
+{
+    Console.Write("[");
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (i != mas.Length - 1)
+        {
+            if (i % 2 != 0) Console.Write($"{symb}{mas[i]},");
+            else Console.Write($"{mas[i]},");
+        }
+        else
+        {
+            if (i % 2 != 0) Console.Write($"{symb}{mas[i]}]");
+            else Console.Write($"{mas[i]}]");
+        }
+    }
+    Console.WriteLine();
+}
