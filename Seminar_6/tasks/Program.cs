151145d9f6e50d3e809cf8e6e99f7cb2c8d5e15a
unknown <itosha35@gmail.com>
2023-01-19 21:13:19 +0300

    Seminar 6 TASKS

---
+++ Seminar_6/tasks/Program.cs
@@ -0,0 +1,69 @@
+﻿/* bool TreangleCheck(int a, int b, int c)
+{
+    if (a < b + c & b < a + c & c < a + b)
+        return true;
+    else return false;
+}
+Console.WriteLine("Treangle ceck Program");
+Console.Write("input a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("input b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("input c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(TreangleCheck(a,b,c) ? "YES it is treangle":"NO, it is not" );
+*/
+/*int[] Fibbonachi(int a, int b, int size)
+{
+    int[] mas = new int[size];
+    mas[0] = a;
+    mas[1] = b;
+    for (int i = 2; i < size; i++)
+        mas[i] = mas[i - 1] + mas[i - 2];
+    return mas;
+}
+void ShowMas(int[] mas)
+{
+    Console.Write("[");
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (i != mas.Length - 1) Console.Write($"{mas[i]}:");
+        else Console.Write($"{mas[i]}]");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Fibbo Program");
+Console.Write("input a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("input b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+*/
+int[] CopyMas(int[] mas)
+{
+    int[] array = new int[mas.Length];
+    for (int i = 0; i < mas.Length; i++)
+        array[i] = mas[i];
+    return array;
+}
+void ShowMas(int[] mas)
+{
+    Console.Write("[");
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (i != mas.Length - 1) Console.Write($"{mas[i]}:");
+        else Console.Write($"{mas[i]}]");
+    }
+    Console.WriteLine();
+}
+int[] SetMas(int size)
+{
+    int[] mas = new int[size];
+    for (int i = 0; i < size; i++)
+        mas[i] = new Random().Next(0, 11);
+    return mas;
+}
+Console.WriteLine("Program Copy Mas");
+int[] mas1 = SetMas
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("input b = ");
+int b = Convert.ToInt32(Console.ReadLine());
