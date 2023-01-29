fabc81056dd391d12e682e873ca95ad38f7b3071
unknown <itosha35@gmail.com>
2023-01-29 23:18:33 +0300

    TAsk 62 Resolved

--- Seminar_8/Task_62/Program.cs
+++ Seminar_8/Task_62/Program.cs
@@ -15,51 +15,86 @@ void Show2dArray(int[,] mas)
 int[,] FillSpiralArray(int rows, int columns)
 {
     int[,] mas = new int[rows, columns];
-    // cycle can be devided on 4 steps:
-    //left to right = -1 row, stepi
-    //top to down = -1 column, stepj
-    //right to left = -1 row, stepi
-    //down to top = -1 column, stepj
-    byte seq = 1;
-    if (seq == 1) // Row number <i> on HOLD, filling row by columns <j>
+    int dir = 1;
+    int cycle = 0;
+    int num = 0;
+    while (num < rows * columns)
     {
-        for (int j = posj; i < columns - stepj; j++)
-            
-            seq++;
-    }
-    if (seq == 2)
-    {
-        seq++;
-    }
+        if (dir == 1)
+        {
+            for (int j = cycle; j < columns - cycle; j++)
+            {
+                num++;
+                mas[cycle, j] = num;
+                if (num >= rows * columns)
+                {
+                    Console.WriteLine($"Cycles = {cycle}");
+                    return mas;
+                }
+            }            
+            dir = 2;
+        }
+        if (dir == 2)
+        {
+            for (int i = 1 + cycle; i < rows - cycle; i++)
+            {
+                num++;
+                mas[i, columns - cycle - 1] = num;
+                if (num >= rows * columns)
+                {
+                    Console.WriteLine($"Cycles = {cycle}");
+                    return mas;
+                }
+            }            
+            dir = 3;
+        }
 
-    if (seq == 3)
-    {
-        seq++;
-    }
-    if (seq == 4)
-    {
-        seq++;
+        if (dir == 3)
+        {
+            for (int j = columns - 1 - cycle - 1; j >= 0 + cycle; j--)
+            {
+                num++;
+                mas[rows - cycle - 1, j] = num;
+                if (num >= rows * columns)
+                {
+                    Console.WriteLine($"Cycles = {cycle}");
+                    return mas;
+                }
+            }            
+            dir = 4;
+        }
+        if (dir == 4)
+        {
+            for (int i = rows - 1 - 1-cycle; i >= 1 + cycle; i--)
+            {
+                num++;
+                mas[i, cycle] = num;
+                if (num >= rows * columns)
+                {
+                    Console.WriteLine($"Cycles = {cycle}");
+                    return mas;
+                }
+            }            
+            dir = 1;
+            cycle++;
+        }
     }
     return mas;
 }
-
-/*
-Array numbers
-00 01 02 03
-11 12 13 04
-10 15 14 05
-09 08 07 06
-indexes
-00 01 02 03
-10 11 12 13
-20 21 22 23
-30 31 32 33
-Step counts
-step 1 = columns
-step 2 = rows - 1
-step 3 = columns - 1
-step 4 = rows - 2
-step 5 = columns - 2
-step 6 = rows - 3
-step 7 = columns - 3
+/*      EXPLANATION of Sequence
+        //cycle 1
+        moves[0, i] = 0          1         rows rows-1;//posi
+        moves[1, i] = 0       columns columns -1   0;//posj
+        moves[2, i] = columns  rows         0         1;//length
+        //cycle 2
+        moves[0, i] = 1          2         rows - 1     rows - 2;//posi
+        moves[1, i] = 1       columns - 1    columns - 2   1;//posj
+        moves[2, i] = columns - 1  rows - 1         1         2;//len
+        //cycle 3
 */
+Console.Clear();
+Console.WriteLine("Program Spiral Array filling");
+int rows = InputGen("Please, input Rows of 3d Array");
+int columns = InputGen("Please, input Columns of 3d Array");
+int[,] mas = FillSpiralArray(rows, columns);
+Show2dArray(mas);
