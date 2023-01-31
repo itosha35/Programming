9e95a39e88f0ce15969248408a69ebaca7270a90
unknown <itosha35@gmail.com>
2023-02-01 00:03:05 +0300

    Seminar 9 added

---
+++ Seminar_9/Task_66/Program.cs
@@ -0,0 +1,12 @@
+﻿int SumInterval(int n, int m)
+{
+    if (n > m) return m + SumInterval(n, m + 1);
+    if (m > n) return n + SumInterval(n + 1, m);
+    else return n;
+}
+int InputGen(string message)
+{
+    Console.Write(message + " = ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(SumInterval(InputGen("Please, Input N"), InputGen("Please, Input M")));
