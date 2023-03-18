3d241530f886d6c80054315d39b294c6624ace04
unknown <itosha35@gmail.com>
2023-03-18 20:40:38 +0300

    exeption reworked

--- PYTHON/Seminar_2/task_extra_1.py
+++ PYTHON/Seminar_2/task_extra_1.py
@@ -1,18 +1,19 @@
 import math
 
 #mas = [501, 2, 1, 98, 9, 11, 20, 100, 9978, 1234]
-mas = [61,228,9]
+mas = [61,228,10,0]
 print(mas)
 cmas = []
 n = int(math.log10(max(mas)))
 for i in mas:
-    if int(math.log10(i)) < n:
-        i = i * int(math.pow(10, (n - int(math.log10(i))))) + \
-            int(math.pow(10, (n - int(math.log10(i)))))-1
+    if i != 0:
+        if int(math.log10(i)) < n:
+            i = i * int(math.pow(10, (n - int(math.log10(i))))) + \
+                int(math.pow(10, (n - int(math.log10(i)))))-1
     cmas.append(i)        
 num = ''
 for i in range(len(cmas)):
     k = cmas.index(max(cmas))
     num = num + str(mas[k])
-    cmas[k] = 0    
+    cmas[k] = -1    
 print(num)
