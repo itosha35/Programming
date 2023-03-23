f6ba7702fa14ace01b5cbb1e3b17d58b0594c1b1
unknown <itosha35@gmail.com>
2023-03-23 22:54:30 +0300

    Task 20 resolved

--- PYTHON/Seminar_1/Task_4.py
+++ PYTHON/Seminar_1/Task_4.py
@@ -3,5 +3,6 @@ def CranesAmount(total):
     x = total // 6
     print(f' Petya made {x} \n Sergey made {x} \n Katya made {4*x}')
 
+
 amount = int(input("Please, set total amount of Cranes were made = "))
 CranesAmount(amount)
