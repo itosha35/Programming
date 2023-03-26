2bd6e310f146cce1fe7707ea301ba4f5ba51c8e7
unknown <itosha35@gmail.com>
2023-03-26 23:09:24 +0300

    Extra task 1 solved and updated

--- PYTHON/Seminar_5/task_extra_1.py
+++ PYTHON/Seminar_5/task_extra_1.py
@@ -1,18 +1,18 @@
-def recursion(max1: int, max2: int):
+def secondMax(max1: int, max2: int):
     n = int(input('next = '))
     if (n > 0):
         if (max1 <= n):
-            recursion(n, max1)
+            secondMax(n, max1)
         elif (max2 <= n):
-            recursion(max1, n)
+            secondMax(max1, n)
         else:
-            recursion(max1, max2)
+            secondMax(max1, max2)
     else:
         print(f'Second max = {max2}')
 
 
 def main():
-    recursion(0, 0)
+    secondMax(0, 0)
 
 
 if __name__ == "__main__":
